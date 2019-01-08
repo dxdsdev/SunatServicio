ebb74bba3917b931bd77484b2b709d2919d483bc
Daniel Romero <31458184+croweloper@users.noreply.github.com>
2019-01-07 20:11:29 -0500

    actualizacion

---
+++ slnSunatAplicacion/Ferreteria_AD/Producto_AD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ferreteria_BE;
+
+namespace Ferreteria_AD
+{
+    public class Producto_AD
+    {
+        public string AdministrarProducto(int pIdProducto, string pDescripcion, int pTipoProducto, int pEstado, string pUsuario) {
+            
+            string vRespuesta = "Producto Actualizado";
+            try
+            {
+                var bd = new BD_FerreteriaEntities();
+                bd.Usp_AdministrarProducto(pIdProducto, pDescripcion, pTipoProducto, pEstado, pUsuario);
+                if (pIdProducto==0)
+                {
+                    vRespuesta = "Producto Agregado";
+                }
+                return vRespuesta;
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message.ToString();
+            }
+        }
+    }
+}
